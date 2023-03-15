e51d2baad25556b8338ed0776c0ba2c15d1d172f
SOTUNBO MICHAEL <mikasotunbo@gmail.com>
2023-03-15 21:33:11 +0100

    Create a private route and custom hook for protecting the profile page and add the log out functionality

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -8,14 +8,14 @@ export default function ForgotPassword() {
   function onChange(e) {
     setEmail(e.target.value);
   }
-  async function onSubmit(e){
-    e.preventDefault()
+  async function onSubmit(e) {
+    e.preventDefault();
     try {
-      const auth= getAuth()
-      await sendPasswordResetEmail(auth, email)
-      toast.success("Email was sent ")
+      const auth = getAuth();
+      await sendPasswordResetEmail(auth, email);
+      toast.success("Email was sent ");
     } catch (error) {
-      toast.error("Could not send reset password")
+      toast.error("Could not send reset password");
     }
   }
 
