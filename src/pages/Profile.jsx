25bbdc67f49f15abf307ec7e4f37e1cdeba8736b
SOTUNBO MICHAEL <mikasotunbo@gmail.com>
2023-03-19 00:05:35 +0100

    add the edit functionality to the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,46 @@
-import { getAuth } from "firebase/auth";
-import React, { useState } from "react"
+import { getAuth, updateProfile } from "firebase/auth";
+import { doc, updateDoc } from "firebase/firestore";
+import React, { useState } from "react";
 import { useNavigate } from "react-router";
-
+import { toast } from "react-toastify";
+import { db } from "../Firebase";
 export default function Profile() {
-
-  const auth= getAuth();
-  const navigate= useNavigate();
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const [changeDetail, setChangeDetail] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
-  function onLogOut(){
+  function onLogOut() {
     auth.signOut();
     navigate("/");
   }
   const { name, email } = formData;
+  function onChange(e) {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  }
+  async function onSubmit() {
+    try {
+      if (auth.currentUser.display !== name) {
+        //update the display name in firebase authentication
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+        //update name in the firestore
+        const docRef = doc(db, "users", auth.currentUser.uid);
+        await updateDoc(docRef, {
+          name,
+        });
+      }
+      toast.success("Profile details Update successful");
+    } catch (error) {
+      toast.error("could not update profile detail");
+    }
+  }
   return (
     <div>
       <section
@@ -34,11 +60,14 @@ export default function Profile() {
               type="text"
               id="name"
               value={name}
-              disabled
-              className="mb-6 w-full px-4 py-2 text-xl
+              disabled={!changeDetail}
+              onChange={onChange}
+              className={`mb-6 w-full px-4 py-2 text-xl
                text-gray-700 bg-white border
                border-gray-300 
-                rounded transition ease-in-out"
+                rounded transition ease-in-out ${
+                  changeDetail && "bg-red-200 focus:bg-red-200"
+                }`}
             />
 
             {/*EMAIL INPUT */}
@@ -46,25 +75,39 @@ export default function Profile() {
               type="email"
               id="email"
               value={email}
-              disabled
+              disabled={!changeDetail}
+              onChange={onChange}
               className="mb-6 w-full px-4 py-2 text-xl
              text-gray-700 bg-white border
              border-gray-300 
               rounded transition ease-in-out"
             />
 
-
-<div className="flex justify-between 
-whitespace-nowrap text-sm sm: text-lg mb-6 ">
-<p className="flex items-center 
-">Do you want to change your name?
-<span className="text-red-600 hover:text-red-700
- transition ease-in-out duration-200 ml-1 cursor-pointer">Edit</span></p>
-<p onClick={onLogOut} className="text-blue-600 hover:text-blue-800
- transition duration-200 ease-in-out cursor-pointer">Sign Out</p>
-
-</div>
-
+            <div
+              className="flex justify-between 
+whitespace-nowrap text-sm sm: text-lg mb-6 "
+            >
+              <p className="flex items-center">
+                Do you want to change your name?
+                <span
+                  onClick={() => {
+                    changeDetail && onSubmit();
+                    setChangeDetail((prevState) => !prevState);
+                  }}
+                  className="text-red-600 hover:text-red-700
+ transition ease-in-out duration-200 ml-1 cursor-pointer"
+                >
+                  {changeDetail ? "Apply change" : "Edit"}
+                </span>
+              </p>
+              <p
+                onClick={onLogOut}
+                className="text-blue-600 hover:text-blue-800
+ transition duration-200 ease-in-out cursor-pointer"
+              >
+                Sign Out
+              </p>
+            </div>
           </form>
         </div>
       </section>
@@ -72,3 +115,6 @@ whitespace-nowrap text-sm sm: text-lg mb-6 ">
   );
 }
 //still works before the auth
+// review on git hub
+
+// 8:36
